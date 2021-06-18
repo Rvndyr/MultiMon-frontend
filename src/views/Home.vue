65883601547c7f6d7bb7fe530cb8cdb7144d2394
Randy Rodriguez <Randyr2125@msn.com>
2021-06-17 21:02:44 -0400

    added Auth link and post route for user auth

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+      <h1>Authorize Twitch</h1>
+      <a
+        href="https://id.twitch.tv/oauth2/authorize?client_id=eliq1ssshmd7dc0z0ohal5zz8pszlc&redirect_uri=http://localhost:8080&response_type=code&scope=viewing_activity_read"
+      >
+        Authorize Twitch
+      </a>
+    </div>
+    <h1>{{ message }}</h1>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<style></style>
 
+<script>
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      twitchAccessToken: null,
+    };
   },
+  created: function () {
+    var twitchCode = this.$route.query.code;
+    console.log("The twitch Code is ", twitchCode);
+    if (twitchCode) {
+      var params = { Code: twitchCode };
+      axios.post("http://localhost:3000/twitch_authorize", params).then((response) => {
+        console.log("twitch access token", response.data);
+        localStorage.setItem("twitch_access_token", response.data.access_token);
+        this.$router.push("/");
+      });
+    }
+  },
+  methods: {},
 };
 </script>
